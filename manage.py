ada890a4e6465af8c2a976242a2d837cb580e47b
Nurmuhammad <mashrapov3030@gmail.com>
2023-03-17 15:39:12 +0500

    feat: env and postg

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
 
 def main():
+    load_dotenv()
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
