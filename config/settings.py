a254846a75ac2222358b63b03af6d09b8c576991
Nurmuhammad <mashrapov3030@gmail.com>
2023-03-22 23:11:03 +0500

    feat: add req+

--- config/settings.py
+++ config/settings.py
@@ -196,14 +196,6 @@ SWAGGER_SETTINGS = {
     },
 }
 
-DJOSER = {
-    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
-    'USERNAME_RESET_CONFIRM_URL': 'username/reset/confirm/{uid}/{token}',
-    'ACTIVATION_URL': 'activate/{uid}/{token}',
-    'SEND_ACTIVATION_EMAIL': True,
-    'SERIALIZERS': {},
-
-}
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
